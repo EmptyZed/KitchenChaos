f068e3ae87b87666d0f48e94458db33625568350
EmptyZed <36047478+EmptyZed@users.noreply.github.com>
2024-09-05 00:00:14 -0600

    Tomato Spawn

--- KitchenChaos/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
-    
+
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact() {
         Debug.Log("Interact!");
+        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 
 }
